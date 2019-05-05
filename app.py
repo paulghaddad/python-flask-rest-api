ec81a4dfd6d508f61329b4441e264f635a9a1dc3
Paul Haddad <paulh16@gmail.com>
2019-05-05 09:55:48 -0400

    Get a single book by isbn

--- app.py
+++ app.py
@@ -16,9 +16,22 @@ books = [
 ]
 
 @app.route('/books')
-def hello_world():
+def get_books():
     return jsonify({
         'books': books
     })
+
+@app.route('/books/<int:isbn>')
+def get_book_by_isbn(isbn):
+    return_value = {}
+    for book in books:
+        if book['isbn'] == isbn:
+            return_value = {
+                'name': book['name'],
+                'price': book['price'],
+            }
+
+    return jsonify(return_value)
+
 
 app.run(port=5000)
