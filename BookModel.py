c92d84e2f2c3db365fb173657cd7209f34da0076
Paul Haddad <paulh16@gmail.com>
2019-05-06 07:30:55 -0400

    Add replace_book method

--- BookModel.py
+++ BookModel.py
@@ -47,6 +47,13 @@ class Book(db.Model):
         db.session.commit()
 
 
+    def replace_book(_isbn, _name, _price):
+        book_to_replace = db.session.query(Book).filter(Book.isbn == _isbn).first()
+        book_to_replace.name = _name
+        book_to_replace.price = _price
+        db.session.commit()
+
+
     def __repr__(self):
         book_object = {
             'name': self.name,
