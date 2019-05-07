82e767135b7e6aa7a06ac9a21284c1a082d52df8
Paul Haddad <paulh16@gmail.com>
2019-05-07 06:49:25 -0400

    Add methods for creating and retrieving users and checking credentials.

--- UserModel.py
+++ UserModel.py
@@ -11,3 +11,26 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
 
     password = db.Column(db.String(80), nullable=False)
+
+
+    def username_password_match(_username, _password):
+        user = db.session.query(User).filter_by(username=_username, password=_password).first()
+
+        return bool(user)
+
+
+    def get_all_users():
+        return db.session.query(User).all()
+
+
+    def create_user(_username, _password):
+        new_user = User(username=_username, password=_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return str({
+            'username': self.username,
+            'password': self.password
+        })
